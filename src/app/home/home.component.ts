c4062317f42125d8af3d178ac05d1e4f5a9b3b8b
takiii20 <trishaanncempron@gmail.com>
2023-02-21 17:16:54 +0800

    created tags component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
-import { ActivatedRoute } from '@angular/router'; 
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  foods: Food[] = []; 
+  foods: Food[] = [];
   constructor(private foodService:FoodService, private route:ActivatedRoute){}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       if(params["searchTerm"])
-        this.foods = this.foodService.getAll().filter(food => 
-            food.name.toLowerCase().includes(params["searchTerm"].toLowerCase()));
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+      else if(params['tag'])
+        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
       else
         this.foods = this.foodService.getAll();
     })
-    
+
     this.foods = this.foodService.getAll();
   }
 }
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Food } from '../../shared/models/Food';
+import { Tag } from '../../shared/models/Tag';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,26 @@ export class FoodService {
 
   constructor() { }
 
-  getAllFoodsByTag(tag: string): Food[]{
+  getAllFoodsBySearchTerm(searchTerm: string): Food[]{
+    return this.getAll().filter(food =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
 
-    return tag == "All"? 
+  getAllTags():Tag[]{
+    return[
+      { name: 'All', count: 14 },
+      { name: 'FastFood', count: 4 },
+      { name: 'Pizza', count: 2 },
+      { name: 'Lunch', count: 3 },
+      { name: 'SlowFood', count: 2 },
+      { name: 'Hamburger', count: 1 },
+      { name: 'Fry', count: 1 },
+      { name: 'Soup', count: 1 },
+    ]
+  }
+
+  getAllFoodsByTag(tag: string): Food[]{
+    return tag == "All"?
     this.getAll():
     this.getAll().filter(food => food.tags?.includes(tag));
   }
